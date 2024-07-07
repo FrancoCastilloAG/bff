a99b59051c0b1bc336c9a4997cc7e6deda92f505
FrancoCastilloAG <franco.castillo2811@gmail.com>
2024-07-07 17:52:52 -0400

    intento 3 deploy a k8s

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,4 +9,4 @@ import { LoginModule } from './login/login.module';
   providers: [],
 })
 export class AppModule {}
-//esto es una prueba de jenkins para deploy en kubernetes 2 
+//esto es una prueba de jenkins para deploy en kubernetes 3
