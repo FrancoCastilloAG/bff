8928a863429125df13c12553b0ba53aac5a4b0c8
FrancoCastilloAG <franco.castillo2811@gmail.com>
2024-07-07 18:21:07 -0400

    jenkins con deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,4 +9,4 @@ import { LoginModule } from './login/login.module';
   providers: [],
 })
 export class AppModule {}
-//esto es una prueba de jenkins basico
+//esto es una prueba de jenkins con deployment
