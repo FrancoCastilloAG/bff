87eca89b08c2a35a58509a2e2fa655857e8c9b2a
FrancoCastilloAG <franco.castillo2811@gmail.com>
2024-07-07 19:44:58 -0400

    prueba deploy con docker daemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,4 +9,4 @@ import { LoginModule } from './login/login.module';
   providers: [],
 })
 export class AppModule {}
-//esto es una prueba de jenkins con deployment ahora instalado docker
+//esto es una prueba de jenkins con deployment con todo instalado
