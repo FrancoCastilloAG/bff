79e3097db42f8acd56eeb570bdec1272faf5be59
FrancoCastilloAG <franco.castillo2811@gmail.com>
2024-07-07 20:29:58 -0400

    instalacion de kubectl

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,4 +9,4 @@ import { LoginModule } from './login/login.module';
   providers: [],
 })
 export class AppModule {}
-//esto es una prueba de jenkins con deployment con permisos a jenkins 2.0
+//esto es una prueba de jenkins con kubectl instalado
